7b565c98c627c6075c41e70cb1750f269a867516
`priyankapatamsetti2` <priyanka.patamsetti2@gmail.com>
2022-02-13 14:22:14 +0000

    Changes in basic details&customer details

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -1,8 +1,35 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
+import Sinon from 'sinon';
 
-describe('Basic details', () => {
-  // Write test cases inside this block
-  // refer basic-details.js files
+const el = await fixture(html`<basic-details></basic-details>`);
+const form = el.shadowRoot.querySelectorAll('lion-button');
+describe('BasicDetails.js', () => {
+  //const basicDetail = new BasicDetails();
+  it('check for spy', () => {
+    const abc = Sinon.spy(el, "_toDashboard");
+    form[0].click();
+    expect(abc.calledOnce).to.be.true;
+  });
+
+  it('check accessible', () => {
+    expect(el).to.be.accessible;
+  });
+
+  it('check for capture', () => {
+    const fun = Sinon.spy(el, "_captureDetails");
+    form[1].click();
+    expect(fun.calledOnce).to.be.true;
+  });
+
+
+  it('number to words', () => {
+    const val = 10005;
+    let num = 10005;
+    // const func = el._numToWord(val,num);
+    el._numToWord(val, num);
+    //console.log(func);
+    expect(num).to.equal(10005.00);
+  });
+
 });
